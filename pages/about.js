c99cb201528f3d582269c8f239af28f0f57a3f88
parunkov <parunkov@gmail.com>
2023-06-29 12:48:12 +0500

    layout

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,16 @@
+import { MainLayout } from "@/components/MainLayout";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
 export default function About() {
+    const router = useRouter();
     return (
-        <h1>About</h1>
+        <MainLayout>
+            <Head>
+                <title>About</title>
+            </Head>
+            <h1>About</h1>
+            <button onClick={() => router.push('/')}>Go back to home</button>
+        </MainLayout>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import { MainLayout } from "@/components/MainLayout";
+import Head from "next/head";
+import Link from "next/link";
+
 export default function Index() {
-    return <h1>Hello</h1>
+    return <MainLayout>
+    <Head>
+        <title>Next index</title>
+        <meta name="keywords" content="nxt, js" />
+        <meta charSet="utf-8" />
+    </Head>
+        <h1>Hello</h1>
+        <p><Link href="/about">About</Link></p>
+        <p><Link href="/posts">Posts</Link></p>
+
+    </MainLayout>
 }
